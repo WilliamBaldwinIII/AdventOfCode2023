55af50123e80e23a7e894015f4dbb198fff321d5
William Baldwin <9921967+WilliamBaldwinIII@users.noreply.github.com>
2023-12-03 23:19:13 -0700

    Day 4

---
+++ 4.fsx
@@ -0,0 +1,103 @@
+﻿open System.Numerics
+
+
+#load "Helpers.fs"
+
+fsi.ShowProperties <- false
+fsi.ShowIEnumerable <- false
+fsi.ShowDeclarationValues <- false
+
+open System
+open Helpers
+
+let fileName = $"4"
+
+let lines = Helpers.readFile fileName
+
+type ScratchCard =
+    { Id: int
+      WinningNumbers: int Set
+      Numbers: int Set }
+
+let getCountWinningNumbers (sc: ScratchCard) =
+    let overlap = Set.intersect sc.WinningNumbers sc.Numbers
+    let len = overlap |> Set.count
+    len
+
+let getWinningScore (sc: ScratchCard) =
+    let len = getCountWinningNumbers sc
+    let power = (len - 1)
+
+    match len with
+    | 0 -> 0
+    | _ -> pown 2 power
+
+
+
+let card = "Card "
+
+let parseNumbers str =
+    str |> String.split " " |> Seq.map int |> set
+
+let parseLine (line: string) =
+    let sansCard = line.Substring(card.Length)
+    let split = sansCard |> String.split ":"
+
+    match split with
+    | [| id; rest |] ->
+        match rest |> String.split "|" with
+        | [| winningNumbersStr; numbersStr |] ->
+            let winningNumbers = winningNumbersStr |> parseNumbers
+            let numbers = numbersStr |> parseNumbers
+
+            { Id = int id
+              WinningNumbers = winningNumbers
+              Numbers = numbers }
+        | other -> failwith $"Invalid input: %A{other}"
+    | other -> failwith $"Invalid input: %A{other}"
+
+let scratchCards = lines |> List.map parseLine
+let scores = scratchCards |> List.map getWinningScore
+let totalScore = scores |> List.sum
+
+printfn "\n\n\n\n\n\n!!!!!!!!!!!!!!!!"
+printfn $"{totalScore}"
+printfn "!!!!!!!!!!!!!!!!\n\n\n\n\n\n"
+
+let rec processAllCards (cardInstanceMap: Map<int, int>) (scs: ScratchCard list) =
+    match scs with
+    | [] -> cardInstanceMap
+    | sc :: rest ->
+        // How many of them do we have?
+        let numInstances = cardInstanceMap[sc.Id]
+
+        // How many cards forward do we get to duplicate?
+        let numWinningNumbers = sc |> getCountWinningNumbers
+
+        let winningCopies =
+            rest
+            |> List.take (min rest.Length numWinningNumbers)
+
+        let cardInstanceMap' =
+            winningCopies
+            |> List.fold
+                (fun ciMap wc ->
+                    let curCopiesTotal = ciMap |> Map.find wc.Id
+                    let newTotal = curCopiesTotal + numInstances
+
+                    ciMap.Add(wc.Id, newTotal))
+                cardInstanceMap
+
+        processAllCards cardInstanceMap' rest
+
+let initialMap =
+    scratchCards
+    |> Seq.map (fun sc -> sc.Id, 1)
+    |> Map.ofSeq
+
+let cardMap = scratchCards |> processAllCards initialMap
+let numCards = cardMap |> Map.values |> Seq.sum
+
+printfn "\n\n\n\n\n\n!!!!!!!!!!!!!!!!"
+printfn $"{numCards}"
+printfn "!!!!!!!!!!!!!!!!\n\n\n\n\n\n"
--- Helpers.fs
+++ Helpers.fs
@@ -8,10 +8,6 @@ let readFile file =
     let filePath = $"./inputs/{file}.txt"
     filePath |> File.ReadAllLines |> List.ofArray
 
-let readExampleFile file =
-    let filePath = $"./inputs/{file}-ex.txt"
-    filePath |> File.ReadAllLines |> List.ofArray
-
 
 module String =
 
@@ -20,7 +16,28 @@ module String =
 
         str.Substring(startIndex, length)
 
+    let split (delimeter: string) (str: string) =
+        str.Split(delimeter, StringSplitOptions.RemoveEmptyEntries)
+
 module List =
     let inline product list =
         list
         |> List.fold (fun total cur -> total * cur) LanguagePrimitives.GenericOne
+
+module Array2D =
+    let getSurrounding arr (x, y) =
+        let xLength = arr |> Array2D.length1
+        let yLength = arr |> Array2D.length2
+
+        let indexes =
+            [ x - 1, y - 1
+              x - 1, y
+              x - 1, y + 1
+              x, y - 1
+              x, y + 1
+              x + 1, y - 1
+              x + 1, y
+              x + 1, y + 1 ]
+            |> List.filter (fun (x, y) -> x >= 0 && y >= 0 && x < xLength && y < yLength)
+
+        indexes
